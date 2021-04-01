16c297de4efb0420b53d34075d1b49b4234832a0
Teller Wetzel <tellerwetzel@yahoo.com>
2021-04-01 11:51:17 -0600

    referance code started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,33 @@
 // Assignment code here
+function generatePassword() {
+  var passwordLength = Number(prompt("Pick a number between 8 and 128."));
+  if (passwordLength < 8 || passwordLength > 128) {
+    generatePassword();
+  }
+  else {
+    options();
+  }
+  console.log(passwordLength);
+}
+var options = function() { 
+ var choices = prompt("Enter 'L' for lowercase, 'U' for uppercase, 'N' for numeric, or 'C' for character.");
+ var passwordSet = "";
+ var choicesTypeLower = choicesTypeLower.toLowerCase();
+ if (choicesTypeLower === "l") {
+   passwordSet = "abcdefghijklmnopqrstuvwxyz";
+ }
+ else if (choicesTypeLower === "u") {
+   passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+ }
+ else if (choicesTypeLower === "n") {
+   passwordSet = "1234567890"
+ }
+ else if (choicesTypeLower === "c") {
+   passwordSet = "~!@#$%^&*()_-+={}[]|<>.?/;:"
+ }  
+ var returnValue = "";
+ for (var i = 0; i < passwordLength; i++)  
+}
 
 
 // Get references to the #generate element
@@ -12,6 +41,18 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+// var passwordInfo = {
+// numeric: function() {
+//   Math.floor(Math.random());
+// },
+// lowerCase: "",  
+// upperCase: "", 
+// characters: "",  
+// numeric: 
+// }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+var n = ["1234567890"]
+var l = ["abcdefghijklmnopqrstuvwxyz"]
+var u = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+var c = ["~!@#$%^&*()_-+={}[]|<>.?/;:"]
